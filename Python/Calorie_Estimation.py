141538bc5b9e7a555d5de9de66dfee9180069a25
Abdur <am7809a@american.edu>
2026-04-18 14:26:40 -0400

    Goal for Calorie Estimate Function

---
+++ Python/Calorie_Estimation.py
@@ -0,0 +1,10 @@
+# Goal of averaging the following equations to calculate
+# accurate daily calorie intake
+
+# -- Equations -- #
+# Miffline-St Jeor Equation
+# Harris-Benedict Equaiton
+# Katch_McArdle Equation
+
+def Calorie_Estimation():
+    pass
