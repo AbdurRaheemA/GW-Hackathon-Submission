e32b2daec62c05afab8b07f3bbaf55912d73ade3
Abdur <am7809a@american.edu>
2026-04-18 22:12:39 -0400

    Created outline for Nutrient_to_Food_Group() function

--- Python/Nutrient_Targets.py
+++ Python/Nutrient_Targets.py
@@ -4,6 +4,7 @@
 # Calculated using the following source
 # https://www.ncbi.nlm.nih.gov/books/NBK610333/
 def Nutrient_Breakdown(user_age, total_calories):
+    # --- Calorie Break Up for those above the age of 18 --- #
     if user_age>=18:
         # Fat 20-35
         Upper_Fat = total_calories*0.35
@@ -22,9 +23,10 @@ def Nutrient_Breakdown(user_age, total_calories):
         Lower_Carbohydrate = total_calories*0.45
 
         # Protein 10-35
-        Uper_Protein = total_calories*0.35
+        Upper_Protein = total_calories*0.35
         Lower_Protein = total_calories*0.10
     
+    # --- Calorie Break Up for those between ages 4 to 17 (inclusive) --- #
     elif user_age>=4:
         # Fat 25-35
         Upper_Fat = total_calories*0.35
@@ -43,10 +45,11 @@ def Nutrient_Breakdown(user_age, total_calories):
         Lower_Carbohydrate = total_calories*0.45
 
         # Protein 10-30
-        Uper_Protein = total_calories*0.30
+        Upper_Protein = total_calories*0.30
         Lower_Protein = total_calories*0.10
 
-    elif user_age<4:
+    # --- Calorie Break Up for those below the age of 4 --- #
+    else:
         # Fat 30-40
         Upper_Fat = total_calories*0.40
         Lower_Fat = total_calories*0.30
@@ -64,23 +67,48 @@ def Nutrient_Breakdown(user_age, total_calories):
         Lower_Carbohydrate = total_calories*0.45
 
         # Protein 5-20
-        Uper_Protein = total_calories*0.20
+        Upper_Protein = total_calories*0.20
         Lower_Protein = total_calories*0.05
 
     return {"Fat": (Lower_Fat, Upper_Fat),
             "Linoleic Acid": (Lower_Linoleic_Acid, Upper_Linoleic_Acid),
             "a Linoleic Acid": (Lower_a_Linoleic_Acid, Upper_a_Linoleic_Acid),
             "Carbohydrates": (Lower_Carbohydrate, Upper_Carbohydrate),
-            "Protein": (Lower_Protein, Uper_Protein)
+            "Protein": (Lower_Protein, Upper_Protein)
             }
 
 # Converts the previously calculated percentages from the 
-# Nutrient_Break_Down() function into human-readable information
+# Nutrient_Breakdown() function into human-readable information
 
 # Calculated using the following source
 # https://www.ncbi.nlm.nih.gov/books/NBK56068/table/summarytables.t4/?report=objectonly
-def Nutrient_to_Food_Group(nutrient_breakdown):
-    pass
+def Nutrient_to_Food_Group(user_age, user_gender, nutrient_breakdown):
+
+    # Converts dictionary to array for easier referencing #
+    for nutrient_group, nutrient_range in nutrient_breakdown:
+        pass
+
+    # --- Calorie to Food Group (g) for those 6 months or younger --- #
+    if user_age<00.5:
+        pass
+    
+    # --- Calorie to Food Group (g) for those 12 months or younger --- #
+    elif user_age<=1:
+        pass
+
+    # --- Calorie to Food Group (g) for those ages 3 or younger --- #
+    elif user_age<=3:
+        pass
+
+    # --- Calorie to Food Group (g) for those ages 8 or younger --- #
+    elif user_age<=8:
+        pass
+
+    elif user_gender == "F":
+        pass
+
+    else:
+        pass
 
 # Converts body weight to suggested water intake
 
