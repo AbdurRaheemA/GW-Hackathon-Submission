bd7a571ee5940caf2775598dc70c47a4a56fc62c
Ashley Totten <tottena17@gmail.com>
2026-04-19 00:23:04 -0400

    All Recipes Data

--- R/main.r
+++ R/main.r
@@ -1,28 +1,32 @@
 library(tidyverse)
 
 
+df <- read_csv("Hackathon_Spreadsheet.csv")
+
+
+
+
+
+
 # Reading in data
-recipe <- read_csv("recipe.csv")
-nutrition1 <- read_csv("recipe_details_part_01(in).csv")
-nutrition2 <- read_csv("recipe_details_part_02(in).csv")
-nutrition3 <- read_csv("recipe_details_part_03(in).csv")
-nutrition4 <- read_csv("recipe_details_part_04(in).csv")
-nutrition5 <- read_csv("recipe_details_part_05(in).csv")
-
-# Combining CSVs into one dataset
-list_nutrition <- list(nutrition1, nutrition2, nutrition3, nutrition4, nutrition5)
-list_clean <- list()
-
-for(i in list_nutrition){
-  ntable <- i |>
-    filter(!is.na(id)) |>
-    filter(nchar(id) == 6)
-  list_clean <- append(list_clean, list(ntable))
-}
-
-nutrition <- list_rbind(list_clean)
+# recipe <- read_csv("recipe.csv")
+# nutrition1 <- read_csv("recipe_details_part_01(in).csv")
+# nutrition2 <- read_csv("recipe_details_part_02(in).csv")
+# nutrition3 <- read_csv("recipe_details_part_03(in).csv")
+# nutrition4 <- read_csv("recipe_details_part_04(in).csv")
+# nutrition5 <- read_csv("recipe_details_part_05(in).csv")
+# 
+# # Combining CSVs into one dataset
+# list_nutrition <- list(nutrition1, nutrition2, nutrition3, nutrition4, nutrition5)
+# list_clean <- list()
+# 
+# for(i in list_nutrition){
+#   ntable <- i |>
+#     filter(!is.na(id)) |>
+#     filter(nchar(id) == 6)
+#   list_clean <- append(list_clean, list(ntable))
+# }
+# 
+# nutrition <- list_rbind(list_clean)
 
 # Cleaning
-# nutrition <- nutrition |>
-#   filter(id != "min B3") |>
-#   select(id, image, title)
